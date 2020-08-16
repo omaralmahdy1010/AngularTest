fedc1a969fa4f6743551c16cc8607ff7b3bc87be
omaralmahdy1010 <omarelmokhtar580@gmail.com>
2020-08-16 11:59:07 +0200

    To shopping list

--- src/app/Components/recipes/Recipe.service.ts
+++ src/app/Components/recipes/Recipe.service.ts
@@ -1,16 +1,38 @@
 import { Recipe } from "./Recipe.model"
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service'
+ 
+@Injectable()
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://rasamalaysia.com/wp-content/uploads/2020/02/honey-garlic-salmon1.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://rasamalaysia.com/wp-content/uploads/2020/02/honey-garlic-salmon1.jpg')
+        new Recipe('A Test Recipe',
+         'This is simply a test',
+          'https://rasamalaysia.com/wp-content/uploads/2020/02/honey-garlic-salmon1.jpg',
+          [
+              new Ingredient('Meat', 1),
+              new Ingredient('French Fries', 20)
+          ]),
+        new Recipe('Another Test Recipe',
+         'This is simply a test', 
+         'https://rasamalaysia.com/wp-content/uploads/2020/02/honey-garlic-salmon1.jpg',
+         [
+             new Ingredient('Koshary',5),
+             new Ingredient('Salsa',10)
+         ])
       ];
+
+      constructor(private slService: ShoppingListService) {}
 
       getRecipes() {
           return this.recipes.slice()
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 }
